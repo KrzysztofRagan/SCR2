87caeffdcc6ae5150d649c1a902d5109c1185b1a
KrzysztofRagan <62708275+KrzysztofRagan@users.noreply.github.com>
2020-12-17 13:12:46 +0100

    Add files via upload

---
+++ Lab6.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+
+
+int main()
+{
+int file;
+char im[100];
+struct stat filestat;
+char *file_in_memory;
+int file_size;
+
+     while(1) 
+     {
+
+
+        if(fork() == 0) 
+      {
+            sleep(2);
+            execlp("display", " ", "-update", "1", "plik", NULL);
+      }
+
+	printf("name of file:\n");
+        scanf("%s", im);
+
+     if(( file = open(im, O_RDWR, S_IRWXO | S_IWUSR)) < 0 ) printf("cant open file");
+       file_size= open("file", O_RDWR | O_CREAT);  
+    
+    if(fstat(file, &filestat) < 0) {
+	    printf("error");
+	    return(1);
+      }
+
+
+    truncate("plik", filestat.st_size);
+    file_in_memory = mmap(NULL, filestat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_size, 0 );
+
+    read(file, file_in_memory,filestat.st_size);
+
+    msync(file_in_memory, filestat.st_size, MS_SYNC);
+    
+    munmap(file_in_memory, filestat.st_size);
+    
+    close(file);
+
+    }
+
+
+}
